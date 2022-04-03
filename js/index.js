895ebc54471276603c0da1246d799af9fc2a6db4
boomblocks <boomblocks@users.noreply.github.com>
2022-04-03 01:04:04 +0000

    deploy: 66489433f106c77cc84ec674f90d41084f61f11d

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ var TestClicked = false;
 var interv = null;
 var colors = {'A':(360/7), 'B':(360/7) * 2, 'C':(360/7) * 3, 'D':(360/7) * 4, 'E':(360/7) * 5, 'F':(360/7) * 6, 'G':0};
 var all_buttons = [null];
+var test_buttons = [null];
 
 function Test(){
 	console.log("running test...");
@@ -30,36 +31,32 @@ function createButton(){
 	let button = document.createElement('div');
 	let bs = button.style;
 	let array_position = NextNull(all_buttons);
-	all_buttons[array_position] = button.id;
 	button.id = "button"+array_position.toString();
+	all_buttons[array_position] = button.id;
+	test_buttons[NextNull(test_buttons)] = button;
 	//button.style = `background-color:${color}; height: 125px; width:125px; border:1px solid black`;
 	bs.backgroundColor = `hsl(${colors['A']}, 100%, 50%)`;
-	bs.height = '125px';
-	bs.width = '125px';
+	bs.height = '120px';
+	bs.width = '120px';
 	//bs.border = '1px solid black';
 	bs.display = 'inline-block';
 	bs.marginLeft = '5px';
-	
-	console.log(button.style);
-	button.addEventListener('mousedown', function () {deleteThis(button.id)}, false);
-	button.addEventListener("touchstart", function () {deleteThis(button.id)}, false);
+	button.addEventListener('mousedown', function () {deleteThis(button.id, button)}, false);
+	button.addEventListener("touchstart", function () {Play()}, false);
 	document.getElementById("toybox").appendChild(button);
 }
 
-function deleteThis(id){
+function deleteThis(id, button){
+	/*
 	var item = document.getElementById(id);
 	item.parentNode.removeChild(item);
+	all_buttons[all_buttons.indexOf(id)] = null;
+	test_buttons[test_buttons.indexOf(button)] = null;
+	console.log(all_buttons);
+	console.log(test_buttons);*/
 }
 
 function shuffle_colors(){
 	colors = {'A':colors["B"], 'B':colors["C"], 'C':colors["D"], 'D':colors["E"],
 		'E':colors["F"], 'F':colors["G"], 'G':colors["A"]};
-}
-
-function lava() {
-	for (c in all_buttons){
-		if (!(c === null)){
-			document.getElementById(c).style.background = 'red';
-		}
-	}
 }
