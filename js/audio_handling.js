be2ffa1af20729befe7b844f19379494dec0df9c
boomblocks <boomblocks@users.noreply.github.com>
2022-04-04 20:10:28 +0000

    deploy: e5bbc1a5ef28d5343693888bf907a53459b30636

---
+++ js/audio_handling.js
@@ -0,0 +1,8 @@
+/*
+Buttons choose a channel with a dropdown menu.
+Synths will choose a channel to send their sound to
+THIS WAY, BUTTONS AND SYNTHS WILL USE THE SAME LIST.
+SYNTHS WORK EXACTLY LIKE THE PIANO MODULE
+
+
+*/
